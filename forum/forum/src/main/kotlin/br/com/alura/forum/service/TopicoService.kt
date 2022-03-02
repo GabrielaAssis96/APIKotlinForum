be77335e37520be8b89c7008b263eb7baff3fcc9
Gabriela Assis <gabriela_assis96@hotmail.com>
2022-03-02 19:28:47 -0300

    Ajustando mapper form

--- forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.service
 
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
@@ -11,9 +12,9 @@ import java.util.stream.Collectors
 @Service
 class TopicoService(
     private var topicos: List<Topico> = ArrayList(),
-    private val cursoService: CursoService,
-    private val usuarioService: UsuarioService,
-    private val topicoViewMapper: TopicoViewMapper
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper
+
 ) {
 
     fun listar(): List<TopicoView> {
@@ -29,18 +30,10 @@ class TopicoService(
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(dto: NovoTopicoForm) {
-        topicos = topicos.plus(
-            Topico(
-                id = topicos.size.toLong() + 1,
-                titulo = dto.titulo,
-                mensagem = dto.mensagem,
-                curso = cursoService.buscarPorId(dto.idCurso),
-                autor = usuarioService.buscarPorId(dto.idAutor)
-
-            )
-
-        )
+    fun cadastrar(form: NovoTopicoForm) {
+        val topico = topicoFormMapper.map(form)
+        topico.id = topicos.size.toLong() + 1
+        topicos = topicos.plus(topico)
     }
 }
 
