e72152a3652c5138cc5aee6e1d107dd09131e5c0
Gabriela Assis <gabriela_assis96@hotmail.com>
2022-03-02 19:13:50 -0300

    Criando mais dtos

--- forum/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,6 +1,7 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.dto.NovoTopicoDto
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.Topico
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.*
@@ -10,17 +11,17 @@ import org.springframework.web.bind.annotation.*
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
-    fun lista(): List<Topico> {
+    fun lista(): List<TopicoView> {
         return service.listar()
     }
 
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id: Long): Topico {
+    fun buscarPorId(@PathVariable id: Long): TopicoView {
         return service.buscarPorId(id)
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody topico: NovoTopicoDto){
+    fun cadastrar(@RequestBody topico: NovoTopicoForm){
         return service.cadastrar(topico)
     }
 
--- forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,28 +1,35 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.dto.NovoTopicoDto
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
 import java.util.*
+import java.util.stream.Collectors
 
 @Service
 class TopicoService(
     private var topicos: List<Topico> = ArrayList(),
     private val cursoService: CursoService,
-    private val usuarioService: UsuarioService
+    private val usuarioService: UsuarioService,
+    private val topicoViewMapper: TopicoViewMapper
 ) {
 
-    fun listar(): List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map { t ->
+            topicoViewMapper.map(t)
+        }.collect(Collectors.toList())
     }
 
-    fun buscarPorId(id: Long): Topico {
-        return topicos.stream().filter { t ->
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = topicos.stream().filter { t ->
             t.id == id
         }.findFirst().get()
+        return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(dto: NovoTopicoDto) {
+    fun cadastrar(dto: NovoTopicoForm) {
         topicos = topicos.plus(
             Topico(
                 id = topicos.size.toLong() + 1,
