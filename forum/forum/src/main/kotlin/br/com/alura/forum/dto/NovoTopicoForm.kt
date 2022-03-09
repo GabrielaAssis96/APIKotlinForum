169b5b69a7c503e5839a0ebeac6e9f1a83714072
Gabriela Assis <gabriela_assis96@hotmail.com>
2022-03-09 20:39:43 -0300

    Add dependencias da jpa e banco h2

--- forum/forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -4,9 +4,9 @@ import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 data class NovoTopicoForm(
-    @field:NotEmpty
+    @field:NotEmpty(message = "Campo titulo não poderá ser vazio")
     var titulo: String,
-    @field:NotEmpty
+    @field:NotEmpty(message = "Campo mensagem não poderá ser vazia")
     var mensagem: String,
     @field:NotNull
     var idCurso: Long,
